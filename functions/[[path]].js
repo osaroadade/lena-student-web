696a2d2ae538e891a4c6bd7d45fb58b52c6b71a7
Osaro Ochuko Adade <adadechuks@gmail.com>
2025-08-24 14:51:21 +0100

    fix(cf-pages): use dynamic import for server build
    - Change from static import to dynamic import: () => import('../build/server/index.js')
    - This may help avoid bundling Node.js APIs at build time
    - Cloudflare adapter should handle the dynamic import at runtime

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,11 +1,8 @@
 // Cloudflare Pages Function for React Router v7 SSR
 import { createPagesFunctionHandler } from "@react-router/cloudflare";
 
-// Import the server build - this path should match your build output
-import * as build from "../build/server/index.js";
-
 export const onRequest = createPagesFunctionHandler({
-  build,
+  build: () => import("../build/server/index.js"),
   getLoadContext(context) {
     return {
       // Make Cloudflare context available to loaders/actions
