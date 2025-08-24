43cb718e21402d178c58da9de7069e8f1bff42fe
Osaro Ochuko Adade <adadechuks@gmail.com>
2025-08-24 19:56:26 +0100

    refactor: remove unused Cloudflare Pages middleware and related configurations

--- functions/[[path]].ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import { createRequestHandler } from "@react-router/cloudflare";
-
-export const onRequest = async (context) => {
-    const handleRequest = createRequestHandler(
-        () => import("../build/server/index.js")
-    );
-    return handleRequest(context.request);
-};
